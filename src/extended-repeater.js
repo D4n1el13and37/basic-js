368c6f2bb388e127593d013a43c06a4041889207
D4n1el13and37 <daniel837@mail.ru>
2023-12-21 11:21:42 +0300

    feat: solve repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const origingalRepeat = options.repeatTimes;
+  const origingalSeparator = options.separator;
+  const addString = options.addition;
+  const addStringRepeatTime = options.additionRepeatTimes;
+  const addSeparator = options.additionSeparator;
+
+  let answer = str; //в ответ записали строку
+  let addition = addString; //по анналогии создаем добавку
+  
+  /**
+   * считаем добавку(её вид)
+   */
+  for (let i = 1; i < addStringRepeatTime; i++) {
+    if (addSeparator) {
+      addition += `${addSeparator}${addString}`
+    } else {
+      addition += `|${addString}`;
+    }
+  }
+  
+  //если добавка есть сразу ее добавляем
+  if (addition) {
+    answer += addition
+  }
+
+  //высчитываем итог строки добавляя разделители и добавку по необходимости
+  for (let i = 1; i < origingalRepeat; i++) {
+    if (origingalSeparator) {
+      answer += `${origingalSeparator}${str}`
+    } else {
+      answer += `+${str}`;
+    }
+
+    if (addition) {
+      answer += addition
+    }
+  }
+
+  return answer;
 }
 
 module.exports = {
