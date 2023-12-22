b7bc5abfbd1443c64728988399a71a8ca72ec08c
D4n1el13and37 <daniel837@mail.ru>
2023-12-22 13:43:47 +0300

    feat: compleate vigenere-cihper

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,104 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(directionRight = true, alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ") {
+    this.directionRight = directionRight;
+    this.alphabet = alphabet;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  /**
+   * @param {String} char letter in alphabet
+   * @param {String} key letter from keywords
+   * @param {Boolean} encrypt check encrypt or decrypt char
+   * @returns encrypted/decrypted char
+   */
+  charСryptography(char, key, encrypt) {
+    const charIdx = this.alphabet.indexOf(char);
+    const keyIdx = this.alphabet.indexOf(key);
+    const alphaLength = this.alphabet.length;
+
+    if (charIdx === -1) {
+      return char;
+    }
+    const encriptedChar = (charIdx + (encrypt ? 1 : -1) * keyIdx + alphaLength) % alphaLength;
+    return this.alphabet[encriptedChar];
+  }
+
+  /**
+   * @param {String} message message which we would cryptography
+   * @param {String} keyPhrase key phrase by which the message is subject to cryptography
+   * @param {Boolean} encryption check encrypt or decrypt message
+   * @returns {String} cryptography answer
+   */
+  messageCryptography(message, keyPhrase, encryption) {
+    let cryptographyAnswer = "";
+    let keyIdx = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const char = message[i].toUpperCase();
+      const keyChar = keyPhrase[keyIdx % keyPhrase.length].toUpperCase();
+      const cryptoChar = this.charСryptography(char, keyChar, encryption);
+      cryptographyAnswer += cryptoChar;
+      
+      if (this.alphabet.includes(char)) {
+        keyIdx += 1;
+      }
+    }
+    return cryptographyAnswer;
+  }
+
+  /**
+   * @param {String} phrase message which we shoud encode
+   * @param {String} key phrase by which the shoud message is cryptography
+   * @returns encoded message
+   */
+  encrypt(phrase, key) {
+    try {
+      const encrypted = this.messageCryptography(phrase, key, true);
+      return this.directionRight
+        ? encrypted
+        : encrypted.split("").reverse().join("");
+    } catch (e) {
+      throw new Error("Incorrect arguments!");
+    }
+  }
+
+  /**
+   * @param {String} phrase message which we shoud decode
+   * @param {String} key phrase by which the shoud message is decode
+   * @returns decrypted phrase
+   */
+  decrypt(phrase, key) {
+    try {
+      const dencrypted = this.messageCryptography(phrase, key, false);
+      return this.directionRight
+        ? dencrypted
+        : dencrypted.split("").reverse().join("");
+    } catch (e) {
+      throw new Error("Incorrect arguments!");
+    }
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
